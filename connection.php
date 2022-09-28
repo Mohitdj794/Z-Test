7f5f81ec0a50825fd93f9d461a172efa1bd11911
Mohitdj794 <mohit.krishnanraju@ziffity.com>
2022-09-28 20:35:39 +0530

    change the database password

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ class Connection
 {
   private $serverName = "localhost";
   private $userName = "root";
-  private $password = "Vignesh@21";
+  private $password = "Ziffity@123";
   private $database = "z_Test";
   protected $con;
 
