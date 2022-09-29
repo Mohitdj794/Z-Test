81b8fe925414f534509839245f14d6eee8474c57
Mohitdj794 <mohit.krishnanraju@ziffity.com>
2022-09-29 21:03:20 +0530

    remove

--- View/Delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "../Class/Query.php";
-    $objd=new Query();
-   $objd->DeleteThis($_GET['id']);
-
-?>
--- responseFile/userExamDetail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    require_once '../Class/Query.php';
-    // $useName = $_POST["user"];
-    $UserExamDetail = new Query();
-    $result = $UserExamDetail->fetchUserDetailFromExamDetail();
-    print_r(json_encode($result));
-        
